8ed4c537aa94711e93b1e833aa4306d75a7d1f49
Hiromitsu Iwata <hiromitsu7@gmail.com>
2021-04-11 21:58:20 +0900

    地面を追加、サンプリングを追加

--- self-made/main.go
+++ self-made/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/rand"
 	"os"
 )
 
@@ -81,22 +82,50 @@ func (r Ray) HitSphere(s Sphere) (bool, Vector, Vector) {
 	}
 }
 
-func (r Ray) Color() Vector {
-	sphere := Sphere{Center: Vector{0.0, 0.0, 1.5}, Radius: 0.5}
-
-	hit, hitPoint, normal := r.HitSphere(sphere)
-
-	// 球に当たった場合
-	if hit {
-		if math.Abs(hitPoint.X) < 0.01 && math.Abs(hitPoint.Y) < 0.01 {
-			fmt.Println(hitPoint)
-		}
-		return Vector{0.5*normal.Y + 0.5, 0.0, 0.0}
+func (r Ray) HitGround() (bool, Vector) {
+	isHit := false
+	if r.Direction.Y < 0.0 {
+		isHit = true
 	}
 
-	// 水平線上の場合の場合
-	if math.Abs(r.Origin.Y-r.Direction.Y) < 0.001 {
-		return Vector{0.0, 1.0, 0.0}
+	hitPoint := Vector{-r.Origin.Y / r.Direction.Y * r.Direction.X, 0.0, -r.Origin.Y / r.Direction.Y * r.Direction.Z}
+
+	return isHit, hitPoint
+}
+
+func (r Ray) Color() Vector {
+
+	sphere := Sphere{Center: Vector{0.0, 1.0, 2.0}, Radius: 1.0}
+
+	hitSphere, hitSpherePoint, sPhereNormal := r.HitSphere(sphere)
+	hitGround, hitGroundPoint := r.HitGround()
+
+	// 球に当たった場合
+	if hitSphere {
+		if math.Abs(hitSpherePoint.X) < 0.01 && math.Abs(hitSpherePoint.Y) < 0.01 {
+			fmt.Println(hitSpherePoint)
+		}
+		return Vector{0.5*sPhereNormal.Y + 0.5, 0.0, 0.0}
+	}
+
+	// 地面に当たった場合
+	if hitGround {
+		// 球の中心からの距離の2乗
+		len2 := math.Pow(hitGroundPoint.X-sphere.Center.X, 2) + math.Pow(hitGroundPoint.Z-sphere.Center.Z, 2)
+		// 半径の2乗
+		rad2 := math.Pow(sphere.Radius, 2)
+
+		baseColor := Vector{0.0, 0.5, 0.5}
+
+		floorFactor := (hitGroundPoint.X - math.Floor(hitGroundPoint.X) + hitGroundPoint.Z - math.Floor(hitGroundPoint.Z)) / 2.0
+		floorColor := Vector{0.2, 0.2, 0.2}.MultiplyScalar(floorFactor)
+
+		shadowFactor := 1.0
+		if len2 <= rad2 {
+			shadowFactor = (1 - (rad2-len2)/rad2)
+		}
+
+		return baseColor.MultiplyScalar(shadowFactor).Add(floorColor)
 	}
 
 	// 何も当たらなかった場合
@@ -122,6 +151,7 @@ func check(e error, s string) {
 }
 
 func main() {
+
 	nx := 800
 	ny := 400
 
@@ -140,23 +170,27 @@ func main() {
 	lowerLeft := Vector{-2.0, -1.0, 1.0}
 	horizontal := Vector{4.0, 0.0, 0.0}
 	vertical := Vector{0.0, 2.0, 0.0}
-	origin := Vector{0.0, 0.0, 0.0}
+	pointOfView := Vector{0.0, 0.6, 0.0}
 
 	for j := ny - 1; j >= 0; j-- {
 		for i := 0; i < nx; i++ {
-			// 0〜1の値
-			u := float64(i) / float64(nx)
-			v := float64(j) / float64(ny)
-			// 0〜4, 0〜2の座標の点
-			position := horizontal.MultiplyScalar(u).Add(vertical.MultiplyScalar(v))
-			// -2〜2, -1〜1の座標の点
-			direction := lowerLeft.Add(position).Normalize()
+			rgb := Vector{}
+			samplingCount := 500
+			for s := 0; s < samplingCount; s++ {
+				// 0〜1の値
+				u := (float64(i) + rand.Float64()) / float64(nx)
+				v := (float64(j) + rand.Float64()) / float64(ny)
+				// 0〜4, 0〜2の座標の点
+				position := horizontal.MultiplyScalar(u).Add(vertical.MultiplyScalar(v))
+				// -2〜2, -1〜1の座標の点
+				direction := lowerLeft.Add(position).Normalize()
 
-			rgb := Ray{origin, direction}.Color()
+				rgb = rgb.Add(Ray{pointOfView, direction}.Color())
+			}
 
-			ir := int(color * rgb.X)
-			ig := int(color * rgb.Y)
-			ib := int(color * rgb.Z)
+			ir := int(color * rgb.X / float64(samplingCount))
+			ig := int(color * rgb.Y / float64(samplingCount))
+			ib := int(color * rgb.Z / float64(samplingCount))
 
 			_, err = fmt.Fprintf(f, "%d %d %d\n", ir, ig, ib)
 			check(err, "Error writting to file: %v\n")
